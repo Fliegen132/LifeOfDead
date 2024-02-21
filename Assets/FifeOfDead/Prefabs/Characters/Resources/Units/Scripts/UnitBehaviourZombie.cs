0ea28a1dfb4df5087f266d066f591588909a44a0
WIN-4RVMULQSR2F\User <fliegenkun@gmail.com>
2024-02-22 02:34:14 +0700

    AddZombie

---
+++ Assets/FifeOfDead/Prefabs/Characters/Resources/Units/Scripts/UnitBehaviourZombie.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace FifeOfDead.Units
+{
+    public class UnitBehaviourZombie : IUnitBehaviour
+    {
+        public override void BehaviourUnit(float speed)
+        {
+
+        }
+    }
+}
